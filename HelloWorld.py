729d26287ce2bcb46a040889ab8e9800b27cd9fa
AmulyaDebo <90204024+AmulyaDebo@users.noreply.github.com>
2023-01-25 16:25:49 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,11 +2,6 @@
 # This Phython program will print Hellow World...
 print("Hello World Demo...24 Jan 2023...new \n")
 
-print("Hello World Demo...24 Jan 2023...new \n")
-
-print("Hello World Demo...24 Jan 2023...new \n")
-
-print("Hello World Demo...24 Jan 2023...new \n")
 
 
 
